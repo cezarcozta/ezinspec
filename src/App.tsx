b14309aaff2af5c4b87e532c9c299ec5e9296937
cezarcozta <cezarcozta@gmail.com>
2021-05-20 22:06:53 -0300

    main theme init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "../src/styles/Theme";
 import AppProvider from "./contexts/index";
 import Routes from "./routes";
 
 function App() {
   return (
-    <AppProvider>
-      <Routes />
-    </AppProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+    </ThemeProvider>
   );
 }
 
