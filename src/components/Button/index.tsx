cf4410987df35c14869fc6b6bc9882a917b87c5c
cezarcozta <cezarcozta@gmail.com>
2021-05-22 10:40:00 -0300

    buttonComponent

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import { ButtonBaseProps } from "@material-ui/core";
 import { ButtonComponent } from "./styles";
 
-const Button: React.FC<ButtonBaseProps> = ({ children }) => {
-  return <ButtonComponent>{children}</ButtonComponent>;
+const Button: React.FC<ButtonBaseProps> = ({ children, type, onClick }) => {
+  return (
+    <ButtonComponent type={type} onClick={onClick}>
+      {children}
+    </ButtonComponent>
+  );
 };
 
 export default Button;
